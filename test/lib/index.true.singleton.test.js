11dc3fd0cc69b7047b5c861131ff2d052fa679d4
Adnane SAGHIR <adnane.saghir@thomsonreuters.com>
2016-08-05 12:29:23 +0200

    storing all uids in memory for safety

--- test/lib/index.true.singleton.test.js
+++ test/lib/index.true.singleton.test.js
@@ -5,49 +5,45 @@ const assert = chai.assert;
 const OneTool = require('./index.true.singleton.testdata');
 
 const instances = {};
+const uid = {};
 const config = {
   name: 'oneTool',
   properties: {},
-  singleton: true,
 };
 const dependencies = {};
 
 describe('True singleton', () => {
-  it('should return same instance', (done) => {
+  it('should return same instances when singleton set to true', () => {
+    config.singleton = true;
     instances.one = new OneTool(dependencies, config);
     instances.one.foo = 'bar';
-    const uidone = instances.one.uid; // store in memory the uid value after first invocation of constructor
-    // setTimeout(() => {
+    uid.one = instances.one.uid; // store in memory the uid value after first invocation of constructor
     instances.two = new OneTool(dependencies, config);
+    uid.two = instances.two.uid;
     assert.strictEqual(instances.two.foo, 'bar');
-    assert.strictEqual(instances.two.uid, uidone);
+    assert.strictEqual(uid.two, uid.one);
     instances.two.bar = 'foo';
     assert.strictEqual(instances.one.bar, 'foo');
-    // setTimeout(() => {
     instances.three = new OneTool(dependencies, config);
+    uid.three = instances.three.uid;
     assert.strictEqual(instances.three.foo, 'bar');
     assert.strictEqual(instances.three.bar, 'foo');
-    assert.strictEqual(instances.three.uid, uidone);
-    done();
-    // }, 10);
-    // }, 10);
+    assert.strictEqual(uid.three, uid.one);
   });
-  it('should return different instance', () => {
+  it('should return different instances when singleton set to false', () => {
     config.singleton = false;
     instances.ten = new OneTool(dependencies, config);
+    uid.ten = instances.ten.uid;
     assert.notProperty(instances.ten, 'foo');
     assert.notProperty(instances.ten, 'bar');
-    assert.notEqual(instances.ten.uid, instances.one.uid);
-    assert.notEqual(instances.ten.uid, instances.two.uid);
-    assert.notEqual(instances.ten.uid, instances.three.uid);
+    assert.notEqual(uid.ten, uid.one);
     instances.ten.prop = 'prop';
     instances.eleven = new OneTool(dependencies, config);
+    uid.eleven = instances.eleven.uid;
     assert.notProperty(instances.eleven, 'foo');
     assert.notProperty(instances.eleven, 'bar');
     assert.notProperty(instances.eleven, 'prop');
-    assert.notEqual(instances.eleven.uid, instances.one.uid);
-    assert.notEqual(instances.eleven.uid, instances.two.uid);
-    assert.notEqual(instances.eleven.uid, instances.three.uid);
-    assert.notEqual(instances.eleven.uid, instances.ten.uid);
+    assert.notEqual(uid.eleven, uid.one);
+    assert.notEqual(uid.eleven, uid.ten);
   });
 });
